2efd377918e74d8236a96ef7c886d009c9ac5ea1
Valentina Ismi Febriana <108646998+valentinaif@users.noreply.github.com>
2022-10-10 20:49:23 +0700

    Add files via upload

---
+++ 21. IF dan ELSE Statement.py
@@ -0,0 +1,28 @@
+# If dan Else Statement
+
+# 1. If nya
+# 2. Kondisinya
+# 3. Aksinya
+
+nama = input("Siapa nama anda? ")
+
+# Program sebelumnya
+# If kondisi : aksi
+# Program selanjutnya
+
+# 1. Program if inline
+if nama == "ucup" : print("Kamu ganteng abiezz!!!")
+print(f"Terima kasih {nama}")
+
+# 2. Program if indentation
+if nama == "ucup" :
+    print("Kamu ganteng abiezz!!!")
+    print("Kamu juga keren banget!")
+print(f"Terima kasih {nama}")    
+
+# 3. Else Statement
+if nama == "otong" :
+    print("hai otong si keren!!!")
+else :
+    print("Ah kamu bukan otong, kamu nggak keren!!!")
+print("Akhir dari program")
