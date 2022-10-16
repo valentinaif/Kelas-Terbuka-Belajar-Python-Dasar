776fe02d3e179e988b20908527d204dcbfcc1d68
Valentina Ismi Febriana <108646998+valentinaif@users.noreply.github.com>
2022-10-16 13:26:41 +0700

    Add files via upload

---
+++ 22. ELIF Statement.py
@@ -0,0 +1,14 @@
+# ELIF : ELSE IF STATMENT
+
+nama = input("Nama kamu siapa? ")
+
+if nama == "firdaus" : # kondisi 1
+	print("Hai ganteng!") # aksi true 1
+elif nama == "imaduddin" : # kondisi 2
+	print("Hai si kece abis!") # aksi true 2
+elif nama == "ibrahim" : # kondisi 3
+	print("Hai humoris!") # aksi true 3
+else : 
+	print("Sorry nggak kenal") # aksi false
+print("Akhir dari program")
+	
